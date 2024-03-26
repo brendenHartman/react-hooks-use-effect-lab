c71b40fd79fd86de3508e1d35a4cc48e4d051178
Brenden Hartman <brendenhart22@gmail.com>
2024-03-25 20:56:41 -0500

    added a useEffect hook to add the timer side effect of the component rerender

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect,  useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+     setTimeRemaining(timeRemaining - 1)
+     if(timeRemaining === 1){
+      setTimeRemaining(10)
+      onAnswered(false)
+     }
+    }, 1000)
+
+
+
+    return function cleanup(){
+      clearTimeout(timeoutID)
+    } 
+    
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
